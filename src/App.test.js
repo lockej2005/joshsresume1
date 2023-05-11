cc5542a46c53e073802da4b179f14aed7731fd49
lockej2005 <josh@intelligentinstallations.com.au>
2023-05-11 12:59:43 +1000

    changes to router

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom'; // Add this import
 import App from './App';
 
 test('renders learn react link', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  );
+  render(<App />);
   const linkElement = screen.getByText(/Resume/i); // Modify the text to match the link text
   expect(linkElement).toBeInTheDocument();
 });
