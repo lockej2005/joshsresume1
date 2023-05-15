a3560aa838644ec25db403e4f7e311c22b52222c
lockej2005 <josh@intelligentinstallations.com.au>
2023-05-15 10:56:18 +1000

    chat gpt retard

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'; // Import your CSS file
 import Home from './home';
 import Resume from './resume';
 import Day from './day';
 import Admin from './admin';
-import routes from './routes.json';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        {routes.map((route) => (
-          <Route key={route.path} path={route.path} element={getElement(route.component)} />
-        ))}
-      </Switch>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/" element={<Resume />} />
+        <Route path="/day" element={<Day />} />
+        <Route path="/day/:day" element={<Day />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
-function getElement(component) {
-  const components = {
-    Home,
-    Resume,
-    Day,
-    Admin,
-  };
-
-  return components[component];
-}
