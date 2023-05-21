6b6cc75a8f9ff46429444f3eb36651d71885ee80
lockej2005 <josh@intelligentinstallations.com.au>
2023-05-21 13:35:05 +1000

    fix compile error

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+    moduleNameMapper: {
+      '^axios$': 'axios/index.js',
+    },
+    moduleDirectories: ['node_modules'],
+  };
+  
